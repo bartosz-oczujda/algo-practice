814c25e7c7e9870f2108e17d0d6eff669d525843
Bartosz Oczujda <bartosz.oczujda@gmail.com>
2017-07-18 22:14:19 +0200

    Implement removing duplicates from an array

--- other/remove_duplicates.js
+++ other/remove_duplicates.js
@@ -1,5 +1,28 @@
 // remove duplicates from an array
 
+const removeDuplicates = (arr) => {
+
+    let hash = new Map()
+    let newArr = []
+
+    for(val of arr) {
+        hash.set(val, true)
+    }
+
+    for([key, val] of hash) {
+        newArr.push(key)
+    }
+
+    return newArr;
+
+} 
 
 
-const input = []
+const removeDuplicatesRecursive = (arr) => {
+    // TODO
+}
+
+const input = [5, 5, 6, 7, 1, 1, 4, 2, 2]
+const output = removeDuplicates(input)
+
+console.log(output)
