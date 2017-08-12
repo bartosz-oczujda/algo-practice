a40fee00773737af68816076ce500ea444174cd8
Bartosz Oczujda <bartosz.oczujda@gmail.com>
2017-08-12 19:09:30 +0200

    Removed unnecessary function for getting word boundaries

--- interview_cake/27_reverse_words.js
+++ interview_cake/27_reverse_words.js
@@ -1,13 +1,5 @@
 const reverseWord = (array, wordBounds) => {
     
-    wordBounds = wordBounds || [0, array.length - 1];
-
-    if(wordBounds[0] === wordBounds[1]) {
-        
-        return array;
-
-    }
-
     const steps = Math.floor((wordBounds[1] - wordBounds[0] + 1) / 2);
 
     for(let i = 0; i < steps; i++) {
@@ -20,51 +12,26 @@ const reverseWord = (array, wordBounds) => {
         array[swapIndex] = swapChar;
 
     }
-console.log(wordBounds)
-console.log(array)
+    
     return array;
-
-}
-
-
-const getWordBounds = (array) => {
-
-    const wordBounds = [];
-    let lastSpaceIndex = -1;
-
-    for(let i = 0; i < array.length; i++) {
-        
-        const char = array[i];
-
-        if(char === ' ') {
-
-            wordBounds.push([lastSpaceIndex + 1, i - 1]);
-            lastSpaceIndex = i;
-
-        }
-        else if(i === array.length - 1) {
-
-            wordBounds.push([lastSpaceIndex + 1, i]);
-
-        }        
-
-    }
-
-    return wordBounds;
 
 }
 
 const reverseString = (string) => {
 
     let array = string.split('');
-    array = reverseWord(array);
+    array = reverseWord(array, [0, array.length - 1]);
     
-    const wordBounds = getWordBounds(array);
-    console.log(wordBounds)
+    let charIndex = 0;    
 
-    for(let i = 0; i < wordBounds.length; i++) {
+    for(let i = 0; i <= array.length; i++) {
 
-        array = reverseWord(array, wordBounds[i]);
+        if(array[i] === ' ' || i === array.length) {
+
+            array = reverseWord(array, [charIndex, i - 1]);
+            charIndex = i + 1;
+
+        }
 
     }
 
