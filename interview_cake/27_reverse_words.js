7cd5e6d35a56fdfa3eddf8ce0949e34ef80715f1
Bartosz Oczujda <bartosz.oczujda@gmail.com>
2017-08-12 19:13:43 +0200

    Add exercise description

--- interview_cake/27_reverse_words.js
+++ interview_cake/27_reverse_words.js
@@ -1,3 +1,12 @@
+/*
+You're working on a secret team solving coded transmissions.
+Your team is scrambling to decipher a recent message, worried it's a plot to break into a major European National Cake Vault. The message has been mostly deciphered, but all the words are backwards! Your colleagues have handed off the last step to you.
+
+Write a function reverseWords() that takes a string message and reverses the order of the words in-place. ↴
+
+Since strings in JavaScript are immutable ↴ , we'll first convert the string into an array of characters, do the in-place word reversal on that array, and re-join that array into a string before returning it. This isn't technically "in-place" and the array of characters will cost O(n)O(n) additional space, but it's a reasonable way to stay within the spirit of the challenge. If you're comfortable coding in a language with mutable strings, that'd be even better!
+*/
+
 const reverseWord = (array, wordBounds) => {
     
     const steps = Math.floor((wordBounds[1] - wordBounds[0] + 1) / 2);
