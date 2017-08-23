ee9ba75c7a348fe43c57022f853271e3e9d87e87
Bartosz Oczujda <bartosz.oczujda@gmail.com>
2017-08-23 22:18:03 +0200

    Add initial instructions to selection sort exercise

---
+++ microsoft_dev285x/selection_sort.js
@@ -0,0 +1,11 @@
+const selectionSort = (list) => {
+    
+    //1. Take the full array iterate over it to find the lowest number
+    //2. Swap it with the first one
+    //3. Start step 1 but from the second index the array
+    //4. Repeat
+    
+}
+
+const list = [4,9,7,1,3,6,5];
+
