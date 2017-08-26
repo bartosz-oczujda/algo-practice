468d2ed7355c522e1f42af9fb1ab384bae0376c6
Bartosz Oczujda <bartosz.oczujda@gmail.com>
2017-08-26 18:20:07 +0000

    Add working implementation of selection sort

--- microsoft_dev285x/selection_sort.js
+++ microsoft_dev285x/selection_sort.js
@@ -1,11 +1,39 @@
-const selectionSort = (list) => {
-    
-    //1. Take the full array iterate over it to find the lowest number
-    //2. Swap it with the first one
-    //3. Start step 1 but from the second index the array
-    //4. Repeat
-    
+const selectionSort = (array) => {
+
+    const findMinimum = (start, array) => {
+
+        let min = array[start];
+        let minIndex = 0;
+
+        for(let i = start; i < array.length; i++) {
+
+            if(min > array[i]) {
+
+                min = array[i];
+                minIndex = i;
+
+            }
+
+        }
+        
+        return minIndex;
+
+    }
+
+    for (let i = 0; i < array.length - 1; i++) {
+
+        const minIndex = findMinimum(i, array);
+        const temp = array[minIndex];
+        array[minIndex] = array[i];
+        array[i] = temp;
+
+    }   
+
+    return array;
+ 
 }
 
-const list = [4,9,7,1,3,6,5];
+const array = [4,9,7,1,3,6,5];
+const result = selectionSort(array);
 
+console.log(result); 
